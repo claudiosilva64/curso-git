650493ec8c7f46cb46cf4b12a9e68024908e0162
Claudio Silva <claudio.silva@cloudsolucoestec.com.br>
2020-07-06 14:25:59 -0300

    Trabalhando com Listas

---
+++ PrimeiroProjeto/Employee.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Globalization;
+
+namespace PrimeiroProjeto
+{
+    class Employee
+    {
+    
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double Salary { get; set; }
+
+        public Employee(int id, string name, double salary)
+        {
+            Id = id;
+            Name = name;
+            Salary = salary;
+        }
+
+        public void IncreaseSalary(double percentagem)
+        {
+            Salary += Salary * percentagem / 100; 
+        }
+
+        public override string ToString()
+        {
+             return Id
+                + ", "
+                + Name
+                + ", "
+                + Salary.ToString("F2", CultureInfo.InvariantCulture);
+        }
+
+    }
+
+}
